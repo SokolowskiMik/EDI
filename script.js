8b1b87f405c543d8f84615bd229c41d6043e5e06
anoniarz <s3nkjuu@wp.pl>
2022-12-30 13:05:26 +0100

    a

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=72109a80';
-const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=25ccbe90';
-let y = 0
+const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=72109a80';
+// const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=25ccbe90';
+
 async function getData(x) {
     try {
 
-        y += 1
         const response = await fetch(apiUrl);
         const data = await response.json();
 
         let table = '<table><tr><th>Ranking</th><th>Imie</th><th>Nazwisko</th><th>Plec</th><th>Pochodzenie</th><th>Wiek</th><th>Pseudonim</th><th>Wygrane partie</th><th>Przegrane partie</th></tr>';
 
-        for (let i = 0; i < x + 5 * y; i++) {
+        for (let i = 0; i < x; i++) {
             const item = data[i];
             if (item) {
                 table += `<tr><td>${item.ranking}</td><td>${item.imie}</td><td>${item.nazwisko}</td><td>${item.plec}</td><td>${item.pochodzenie}</td><td>${item.wiek}</td><td>${item.pseudonim}</td><td>${item.wygrane_partie}</td><td>${item.przegrane_partie}</td></tr>`;
@@ -26,7 +25,10 @@ async function getData(x) {
     }
 }
 
-
+async function no_elements() {
+    let number = parseInt(prompt("How many elements:"));
+    getData(number);
+}
 
 async function getData2() {
     const response = await fetch(apiUrl);
@@ -43,52 +45,57 @@ async function getData2() {
         values.push(data[i].ranking);
     }
     new Chart(document.getElementById("bar-chart"), {
-        
         type: 'bar',
         data: {
             labels: labels,
             datasets: [{
                 label: "ranking",
                 backgroundColor: "whitesmoke",
-                data: values
+                data: values,
             }]
         },
         options: {
-            legend: { display: false },
+            legend: {
+                display: false,
+                labels: {
+                    fontColor: "white"
+                }
+            },
             title: {
                 display: true,
-                text: 'ranking graczy'
+                text: 'ranking graczy',
+                fontColor: "white"
             }
         }
     });
-
 }
 
 async function getData3() {
-  const response = await fetch(apiUrl);
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
-  length = data.length;
-  console.log(length);
+    const response = await fetch(apiUrl);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    length = data.length;
+    console.log(length);
 
-  labels = [];
-  values = [];
-  for (i = 0; i < length; i++) {
-      labels.push(data[i].imie);
-      values.push(data[i].wiek);
-  }
-  new Chart(document.getElementById("line-chart"), {
-      
-      type: 'line',
-      data: {
-          labels: labels,
-          datasets: [{
-              label: "wiek",
-              backgroundColor: "whitesmoke",
-              data: values
-          }]
-      },
-  });
+    labels = [];
+    values = [];
+    for (i = 0; i < length; i++) {
+        labels.push(data[i].imie);
+        values.push(data[i].wiek);
+    }
+    new Chart(document.getElementById("line-chart"), {
+
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "wiek",
+                backgroundColor: "whitesmoke",
+                data: values
+
+            }]
+        },
+    });
 
 }
