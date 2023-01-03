2adf50968419d1a23377a3b25bd34398668e4e54
anoniarz <s3nkjuu@wp.pl>
2023-01-03 19:49:35 +0100

    wykresy wypieki

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-// const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=72109a80';
-const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=25ccbe90';
+const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=72109a80';
+// const apiUrl = 'https://my.api.mockaroo.com/yenga.json?key=25ccbe90';
 
 
 // pokazanie wybranej ilosci danych
 async function no_elements() {
     let number = parseInt(prompt("How many results (out of 100)?")); 
     getData(number);
+}
+
+// zamienia liste w slownik
+function counter(arr) {
+    const count = {"male": 0, "female": 0};
+    arr.forEach(item => {
+      if (item in count) {
+        count[item] += 1;
+      } else {
+        count[item] = 1;
+      }
+    });
+    return count;
+  }
+
+// ile dziesiatek lat
+function ages(arr) {
+    const count = {
+        "10's": 0,
+        "20's": 0,
+        "30's": 0,
+        "40's": 0,
+        "50's": 0,
+        "60's+": 0,
+    };
+
+    arr.forEach(item => {
+        if (item >= 10 && item < 20) {
+        count["10's"] += 1;
+        } else if (item >= 20 && item < 30) {
+        count["20's"] += 1;
+        } else if (item >= 30 && item < 40) {
+        count["30's"] += 1;
+        } else if (item >= 40 && item < 50) {
+        count["40's"] += 1;
+        } else if (item >= 50 && item < 60) {
+        count["50's"] += 1;
+        } else if (item >= 60) {
+        count["60's+"] += 1;
+        }
+    });
+    return count;
 }
 
 // kolory
@@ -52,10 +94,8 @@ async function getData(x) {
     labels2 = [];
     values2 = [];
 
-    labels3 = [];
     values3 = [];
 
-    labels4 = [];
     values4 = [];
 
     for (i = 0; i < x; i++) {
@@ -64,7 +104,12 @@ async function getData(x) {
 
         labels2.push(data[i].pseudonim);
         values2.push(data[i].przegrane_partie);
+
+        values3.push(data[i].plec);
+
+        values4.push(data[i].wiek);
     }
+
 
     // ustawienia wykresu slupkowego
     new Chart(document.getElementById("bar-chart"), {
@@ -104,7 +149,6 @@ async function getData(x) {
         }
     });
     
-
     // Ustawienia wykresu liniowego
     new Chart(document.getElementById("line-chart"), {
 
@@ -146,5 +190,72 @@ async function getData(x) {
             }]
         },
     });
+
+    // wykres donut
+    values3 = counter(values3)
+    new Chart(document.getElementById("donut-chart"), {
+
+        options: {
+            layout: { padding: 10 },
+            legend: {
+                labels: {
+                    fontColor: "#f8f9fa",
+                    fontSize: 14,
+                }
+            },
+
+        },
+
+        type: 'doughnut',
+        data: {
+
+            labels: Object.keys(values3),
+            datasets: [{
+                backgroundColor: [
+                    "#22223B",
+                    "#4A4E69",
+                    "#9A8C98",
+                    "#C9ADA7",
+                    "#F2E9E4",
+                    "#22223B",
+                    "#4A4E69",
+                    "#9A8C98",
+                    "#C9ADA7",
+                    "#F2E9E4",
+                ],
+                data: Object.values(values3)
+
+            }]
+        },
+    });
+
+    // wykres ciasto
+    values4 = ages(values4)
+    new Chart(document.getElementById("pie-chart"), {
+
+        options: {
+            layout: { padding: 10 },
+            legend: {
+                labels: {
+                    fontColor: "#f8f9fa",
+                    fontSize: 14,
+                }
+            },
+
+        },
+
+        type: 'pie',
+        data: {
+
+            labels: Object.keys(values4),
+            datasets: [{
+                backgroundColor: colors,
+                data: Object.values(values4)
+
+            }]
+        },
+    });
+        
+
 
 }
